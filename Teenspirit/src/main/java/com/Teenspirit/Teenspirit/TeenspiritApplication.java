aab907e8425021dff8f691df2c377bd9d49d2731
mjiid <habush1610@gmail.com>
2023-11-19 01:24:51 +0100

    Initial commit

---
+++ Teenspirit/src/main/java/com/Teenspirit/Teenspirit/TeenspiritApplication.java
@@ -0,0 +1,13 @@
+package com.Teenspirit.Teenspirit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeenspiritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeenspiritApplication.class, args);
+	}
+
+}
---
+++ Teenspirit/src/test/java/com/Teenspirit/Teenspirit/TeenspiritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Teenspirit.Teenspirit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeenspiritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
